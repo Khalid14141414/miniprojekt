5b714a7d53f22c163003a61d2b31b3d511636f11
khalid behi <khalidfarah86@gmail.com>
2024-03-15 13:54:30 +0100

    gitOpgave

--- src/HelloWorldLinux.java
+++ src/HelloWorldLinux.java
@@ -2,5 +2,7 @@ public class HelloWorldLinux {
     public static void main(String[] args) {
         System.out.println("\nHello World");
         System.out.printf("My name is ovevowe");
+        System.out.println("Directed by master sensei");
     }
+
 }
