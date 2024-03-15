a4fb62f3b8c9a916451b5d0458ca4f6d7320c34b
Natia <nate0001@stud.kea.dk>
2024-03-15 13:51:54 +0100

    Natija

--- src/HelloWorldLinux.java
+++ src/HelloWorldLinux.java
@@ -2,5 +2,6 @@ public class HelloWorldLinux {
     public static void main(String[] args) {
         System.out.println("\nHello World");
         System.out.printf("My name is ovevowe");
+        System.out.println("dlkjfdld");
     }
 }
