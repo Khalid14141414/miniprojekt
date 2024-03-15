e9ac46539c7a0a4ba04c79624931d18b39ee8427
SelimiAli <alse0001@stud.kea.dk>
2024-03-15 14:41:59 +0100

    1 push — by Ali

--- src/HelloWorldLinux.java
+++ src/HelloWorldLinux.java
@@ -1,13 +1,5 @@
 public class HelloWorldLinux {
     public static void main(String[] args) {
-        System.out.println("\nHello World");
-        System.out.printf("My name is ovevowe");
-        System.out.println("NewTest");
-        System.out.println("whaaaattttt?");
-        //daf.kjf-lkjrfl-ke
-        System.out.println("This line of code has been written by Ali");
-        System.out.println("Second line of code / push by Ali");
-        System.out.println("Third line of cod by Ali");
-        System.out.println("ueveveve");
+        System.out.println("1 push — by Ali");
     }
 }
